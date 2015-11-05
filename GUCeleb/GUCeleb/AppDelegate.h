123b2878a7531875549af10d3a9826732adc76f9
Alotaibi, Waleed N <alotaibi026@macpc12.gannon.edu>
2015-11-05 10:01:20 -0500

    Creation of the project

---
+++ GUCeleb/GUCeleb/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GUCeleb
+//
+//  Created by Alotaibi, Waleed N on 11/5/15.
+//  Copyright © 2015 Gannon.edu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GUCeleb/GUCeleb/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GUCeleb
+//
+//  Created by Alotaibi, Waleed N on 11/5/15.
+//  Copyright © 2015 Gannon.edu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
