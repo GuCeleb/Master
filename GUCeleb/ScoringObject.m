faccc643c431db4fed29af0bba721f1e96261215
Masluf, Yousef A <masluf001@macpc12.gannon.edu>
2015-12-05 15:05:56 -0500

    1

---
+++ GUCeleb/ScoringObject.m
@@ -0,0 +1,13 @@
+//
+//  ScoringObject.m
+//  GUCeleb
+//
+//  Created by Masluf, Yousef A on 12/5/15.
+//  Copyright © 2015 Gannon.edu. All rights reserved.
+//
+
+#import "ScoringObject.h"
+
+@implementation ScoringObject
+
+@end
