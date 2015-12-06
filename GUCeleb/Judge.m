bb5c6accf0f5636e37d443a6a1538dd7fdc62e31
Walee <walee@walees-macbook-pro.local>
2015-12-06 18:57:02 -0500

    Judge

---
+++ GUCeleb/Judge.m
@@ -0,0 +1,20 @@
+//#import "Judge.h"
+
+@implementation Judge
+
+// Make an object from NSUserDefaults and init its values
+- (id) initWithCoder: (NSCoder *)coder
+{
+    if (self = [super init])
+    {
+        self.Name = [coder decodeObjectForKey:@"Name"];
+        self.UserID = [coder decodeObjectForKey:@"UserID"];
+        self.Password = [coder decodeObjectForKey:@"Password"];
+        
+    }
+    
+    return self;
+    
+    
+}
+@end
