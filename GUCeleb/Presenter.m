c1645951368dec9f70528a0182d774a92cb69e08
Walee <walee@walees-macbook-pro.local>
2015-12-06 02:08:29 -0500

    Commit 2

--- GUCeleb/Presenter.m
+++ GUCeleb/Presenter.m
@@ -1,13 +1,25 @@
-//
-//  Presenter.m
-//  GUCeleb
-//
-//  Created by Walee on 12/5/15.
-//  Copyright © 2015 Gannon.edu. All rights reserved.
-//
-
 #import "Presenter.h"
 
 @implementation Presenter
 
+// Make an object from NSUserDefaults and init its values
+- (id) initWithCoder: (NSCoder *)coder
+{
+    if (self = [super init])
+    {
+        self.Name = [coder decodeObjectForKey:@"Name"];
+        self.Topic = [coder decodeObjectForKey:@"Topic"];
+        self.Date = [coder decodeObjectForKey:@"Date"];
+        self.Time = [coder decodeObjectForKey:@"Time"];
+        self.Location = [coder decodeObjectForKey:@"Location"];
+        self.image = [coder decodeObjectForKey:@"image"];
+        self.rattings = [coder decodeObjectForKey:@"rattings"];
+        self.isLiked = [coder decodeBoolForKey:@"isLiked"];
+        
+    }
+    
+    return self;
+    
+    
+}
 @end
