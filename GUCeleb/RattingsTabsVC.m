fc7f042ec22337c141a85026f27c641b67dd03ee
Walee <walee@walees-macbook-pro.local>
2015-12-06 22:27:31 -0500

    Fixing Andrew Code

--- GUCeleb/RattingsTabsVC.m
+++ GUCeleb/RattingsTabsVC.m
@@ -24,3 +24,41 @@
         poster = [arr objectAtIndex:1];
         
     }
+    options = [[NSMutableArray alloc] init];
+    
+    // add some sample data
+    [options addObject:@"1"];
+    [options addObject:@"2"];
+    [options addObject:@"3"];
+    [options addObject:@"4"];
+    [options addObject:@"5"];
+    
+    [[IQKeyboardManager sharedManager] setEnableAutoToolbar:NO];
+    [[IQKeyboardManager sharedManager] setEnable:NO];
+    
+    
+    UIBarButtonItem *btn = [[UIBarButtonItem alloc]
+                            initWithBarButtonSystemItem:(UIBarButtonSystemItemStop)
+                            target:self
+                            action:@selector(closeMe:)];
+    
+    btn.tintColor = [UIColor whiteColor];
+    self.navigationItem.leftBarButtonItem = btn;
+    
+    if(_isStudent == NO){
+        UIBarButtonItem *btnSubmit = [[UIBarButtonItem alloc] initWithTitle:@"Submit"
+                                                                      style:UIBarButtonItemStylePlain target:self action:@selector(submitRate:)];
+        btnSubmit.tintColor = [UIColor whiteColor];
+        self.navigationItem.rightBarButtonItem = btnSubmit;
+    }
+    
+    
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+    
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+// Submit rattings for the current presenter
+@end
+
