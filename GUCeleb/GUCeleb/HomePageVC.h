1dcc86754fb82f33672441f95d1d8b1512db866d
Jumana A. H <JAH@jumanas-macbook-pro.local>
2015-12-06 20:55:36 -0500

    Home Page

---
+++ GUCeleb/GUCeleb/HomePageVC.h
@@ -0,0 +1,19 @@
+//
+//  HomePageVC.h
+//  GUCeleb
+//
+//  Created by Jumana A. H. on 11/12/15.
+//  Copyright © 2015 Gannon.edu. All rights reserved.
+//
+
+
+
+#import <UIKit/UIKit.h>
+
+@interface HomePageVC : UIViewController
+
+@property (weak, nonatomic) IBOutlet UIButton *btnJudge;
+@property (weak, nonatomic) IBOutlet UIButton *btnStudents;
+@property (weak, nonatomic) IBOutlet UIButton *btnCommittee;
+
+@end
---
+++ GUCeleb/GUCeleb/HomePageVC.m
@@ -0,0 +1,50 @@
+//
+//  HomePageVC.m
+//  GUCeleb
+//
+//  Created by Jumana A. H. on 11/12/15.
+//  Copyright © 2015 Gannon.edu. All rights reserved.
+//
+
+
+
+#import "HomePageVC.h"
+
+@interface HomePageVC ()
+
+@end
+
+@implementation HomePageVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    [self setNeedsStatusBarAppearanceUpdate];
+    _btnCommittee.layer.cornerRadius = 10;
+    _btnJudge.layer.cornerRadius = 10;
+    _btnStudents.layer.cornerRadius = 10;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (UIStatusBarStyle)preferredStatusBarStyle
+{
+    return UIStatusBarStyleLightContent;
+}
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+    if([segue.identifier isEqualToString:@"JudgeSeague"]){
+        UIViewController *vc = segue.destinationViewController;
+        vc.title = @"Judge Login";
+    }
+}
+
+@end
+
