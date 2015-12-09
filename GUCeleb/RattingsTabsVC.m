894ee00326e6ad489121c6862ec1ce72941aa286
McCracken, Andrew S <mccracke007@macpc13.local>
2015-12-08 23:37:15 -0500

    Push 11/8

--- GUCeleb/RattingsTabsVC.m
+++ GUCeleb/RattingsTabsVC.m
@@ -137,6 +137,83 @@
     return cell;
 }
 
+-(void)dp_Selected:(id)dp {
+    DownPicker *dp1 = (DownPicker *) dp;
+    NSString* selectedValue = [dp1 text];
+    
+    ScoringObject *obj;
+    if(isPlatform){
+        obj = [platform.items objectAtIndex:dp1.tag];
+    }else{
+        obj = [poster.items objectAtIndex:dp1.tag];
+    }
+    obj.Points = [selectedValue intValue];
+    if(isPlatform){
+        [platform.items replaceObjectAtIndex:dp1.tag withObject:obj];
+    }else{
+        [poster.items replaceObjectAtIndex:dp1.tag withObject:obj];
+    }
+}
+
+-(NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section{
+    return isPlatform ? @"Platform" : @"Poster";
+}
+
+- (IBAction)changeView:(id)sender {
+    UISegmentedControl *seg = (UISegmentedControl *) sender;
+    if(seg.selectedSegmentIndex == 0)
+        isPlatform = YES;
+    else
+        isPlatform = NO;
+    
+    [self.tableView reloadData];
+}
+
+
+/*
+ // Override to support conditional editing of the table view.
+ - (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+ // Return NO if you do not want the specified item to be editable.
+ return YES;
+ }
+ */
+
+/*
+ // Override to support editing the table view.
+ - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+ if (editingStyle == UITableViewCellEditingStyleDelete) {
+ // Delete the row from the data source
+ [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+ } else if (editingStyle == UITableViewCellEditingStyleInsert) {
+ // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+ }
+ }
+ */
+
+/*
+ // Override to support rearranging the table view.
+ - (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
+ }
+ */
+
+/*
+ // Override to support conditional rearranging of the table view.
+ - (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
+ // Return NO if you do not want the item to be re-orderable.
+ return YES;
+ }
+ */
+
+/*
+ #pragma mark - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+ // Get the new view controller using [segue destinationViewController].
+ // Pass the selected object to the new view controller.
+ }
+ */
+
 // Submit rattings for the current presenter
 @end
 
