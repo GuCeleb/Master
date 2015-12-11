d4e63d1fec74de911b157b94eef473783927ffd3
Jumana A. H <JAH@jumanas-macbook-pro.local>
2015-12-11 00:15:49 -0500

    Andrew's part story board

---
+++ GUCeleb/GUCeleb/RattingCellVC.m
@@ -0,0 +1,23 @@
+//
+//  RattingCellVC.m
+//  GUCeleb
+//
+//  Created by McCracken, Andrew S on 12/9/15.
+//  Copyright © 2015 Gannon.edu. All rights reserved.
+//
+
+#import "RattingCellVC.h"
+
+@implementation RattingCellVC
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
---
+++ GUCeleb/GUCeleb/StudentsVC.h
@@ -0,0 +1,13 @@
+//
+//  StudentsVC.h
+//  GUCeleb
+//
+//  Created by McCracken, Andrew S on 12/10/15.
+//  Copyright © 2015 Gannon.edu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StudentsVC : UITableViewController
+
+@end
